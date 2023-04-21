3b0182e5c3351270e8ca8a51a05e02aae2f7e369
wjago <s.posnik@gmail.com>
2023-04-21 16:34:32 +0200

    Modification 2

--- src/main/java/pl/zajavka/case1/Animal.java
+++ src/main/java/pl/zajavka/case1/Animal.java
@@ -2,6 +2,7 @@ package pl.zajavka.case1;
 
 public abstract class Animal {
 
+    private String nickname;
     protected String whatIsYourName() {
         return "I don't have any";
     }
--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,5 +1,8 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
-    private String name;
+    @Override
+    public String whatIsYourName() {
+        return "My name is Doggy";
+    }
 }
