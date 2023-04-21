de30388739425920eb398125e0013c380428689e
wjago <s.posnik@gmail.com>
2023-04-21 16:12:28 +0200

    Modification 3

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,8 +1,9 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
-    @Override
-    public String whatIsYourName() {
-        return "My name is Doggy";
+    private String name;
+
+    public void sniff() {
+        System.out.println("I'm soo sniffing!");
     }
 }
